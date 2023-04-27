dfb368a6a9db4e8f2c6c1a0ceef996f46bbfdfb0
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-27 11:02:02 -0700

    added NavBar to main components

--- better-website/src/App.jsx
+++ better-website/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import Hero from "./components/Hero";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="border border-red-500 w-auto flex">
+      <div className="border border-red-500 w-auto flex-col">
         <Hero />
+        <About />
+        <Projects />
+        <Contact />
       </div>
     </>
   );
