a43ef6ae492893d23d3ab6f6912b6058a4632529
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-27 15:53:14 -0700

    realigned text in hero

--- better-website/src/components/Hero.jsx
+++ better-website/src/components/Hero.jsx
@@ -5,16 +5,16 @@ function Hero() {
     <div id="hero-container" className="h-1/2">
       <div
         id="hero-words"
-        className="h-2/5 flex-col text-center pt-6 bg-gradient-to-tr from-gray-600 to-gray-900"
+        className="h-2/5 flex-col text-left pt-6 pl-6 bg-gradient-to-tr from-gray-600 to-gray-900"
       >
-        <h1 className="text-4xl font-black ">
+        <h1 className="text-4xl font-black pb-3">
           Hi, I'm{" "}
           <span className="bg-gradient-to-tr from-cyan-300 to-indigo-700 bg-clip-text text-transparent">
             Josh
           </span>
         </h1>
-        <h2 className="text-xl font-bold">Web Developer</h2>
-        <h3 className="text-sm font-medium">Front-End | Back-End</h3>
+        <h2 className="text-xl font-bold pl-4">Web Developer</h2>
+        <h3 className="text-sm font-medium pl-4">Front-End | Back-End</h3>
       </div>
       <div id="hero-object" className="h-2/3">
         <div className="m-2 h-60 rounded-xl bg-gray-500">&nbsp;</div>
