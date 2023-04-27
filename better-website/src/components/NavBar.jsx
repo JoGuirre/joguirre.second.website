31d2bb034e5403d2a6a76ee20f0a4a54657d1b45
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-27 15:56:55 -0700

    changed bg on navbar

--- better-website/src/components/NavBar.jsx
+++ better-website/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar() {
   return (
     <nav
       id="navbar-container"
-      className="sticky top-0 border-b rounded-b-sm shadow-md shadow-gray-600 bg-blue-900"
+      className="sticky top-0 border-b rounded-b-sm shadow-md shadow-gray-600 bg-transparent backdrop-blur-md"
     >
       <ul className="flex justify-around text-xl font-semibold pt-2 mb-1">
         <li className="px-1 border-hidden rounded-sm hover:bg-gray-200 hover:text-black">
@@ -20,6 +20,7 @@ function NavBar() {
           <a href="#contact">Contact</a>
         </li>
       </ul>
+
       <div className="hidden w-full h-0 flex justify-center absolute top-6">
         <div className="border-b bg-blue-900 rounded-full w-8 h-8">&nbsp;</div>
       </div>
