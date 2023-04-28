321d42c345d59f0597d552d8acb141bf77a3dc6c
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-27 17:51:02 -0700

    about me section finished. need to work on aboutme.json

--- better-website/src/components/About.jsx
+++ better-website/src/components/About.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import aboutMe from "../utilities/aboutMe.json";
 
 function About() {
-  return <div className="h-96">About</div>;
+  return (
+    <div id="about-container" className="">
+      <h1 className="text-center text-3xl font-bold mt-6">About Me</h1>
+      <Paragraph aboutMe={aboutMe} />
+    </div>
+  );
+}
+
+function Paragraph({ aboutMe }) {
+  const titles = aboutMe.reduce((acc, current) => {
+    return [...acc, current.title];
+  }, []);
+
+  const contents = aboutMe.reduce((acc, current) => {
+    return [...acc, current.content];
+  }, []);
+
+  return (
+    <div id="about-paragraph-container" className="px-6 mt-3">
+      {aboutMe.map((section) => {
+        return (
+          <div
+            id="paragraph-container"
+            key={section.key}
+            className="bg-blue-100 rounded-xl pb-2 shadow-inner shadow-lg shadow-blue-900"
+          >
+            <div
+              id="paragraph-title-container"
+              className="pl-4 py-1 bg-blue-800 rounded-lg mb-1 mt-5 shadow-inner shadow-lg shadow-blue-600"
+            >
+              <h3 id="paragraph-title" className="text-xl font-bold">
+                {section.title}
+              </h3>
+            </div>
+            <div id="paragrpah-content-container">
+              <p
+                id="paragraph-content"
+                className="pl-4 pr-2 text-black font-medium"
+              >
+                {section.content}
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default About;
