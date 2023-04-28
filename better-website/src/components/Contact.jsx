28c867db9f7976e40bcf4f40ced8a7045e4400f4
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-04-27 18:02:17 -0700

    added forspree email

--- better-website/src/components/Contact.jsx
+++ better-website/src/components/Contact.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 function Contact() {
-  return <div className="h-96">Contact</div>;
+  const [state, handleSubmit] = useForm("meqwpvaq");
+  const [isOpen, setIsOpen] = useState(false);
+
+  if (state.succeeded && isOpen) {
+    return <JoinedPopup setIsOpen={setIsOpen} />;
+  }
+
+  return (
+    <div id="" className="p-4 m-6 bg-blue-300 rounded-xl">
+      <h1 id="">Contact Me</h1>
+      <div className="">
+        <div className="">
+          <h1>Like what you see?</h1>
+          <h2>Get in touch with me and let's work something out!</h2>
+        </div>
+        <div className="">
+          <form
+            id="contact-form"
+            action="https://formspree.io/f/meqwpvaq"
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+
+            <label htmlFor="subject">Subject:</label>
+            <input type="text" id="subject" name="subject" required />
+
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+
+            <button
+              type="submit"
+              id="submit-button"
+              disabled={state.submitting}
+              onClick={() => setIsOpen(true)}
+            >
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function JoinedPopup({ setIsOpen }) {
+  return (
+    <div className="">
+      <div className="">
+        <h1>Your email has been sent!</h1>
+        <p>
+          <h3>Thank you for your message!</h3>
+          <br />I will be in contact within 2 business days.
+          <br />
+          Please browse my projects!
+        </p>
+        <button id="" onClick={() => setIsOpen(false)}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
--- better-website/src/components/Projects.jsx
+++ better-website/src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Projects() {
-  return <div className="h-96">Projects</div>;
+  return <div className="">Projects</div>;
 }
 
 export default Projects;
