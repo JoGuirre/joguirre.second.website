3c6f99cf37b2aa46d4ff6b6e117e4d79bc4d1178
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-05-01 02:15:20 -0700

    made a kind of carousel, still broken though.

--- better-website/src/components/Hero.jsx
+++ better-website/src/components/Hero.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import codePic from "../assets/code-pic.jpg";
+import codePic1 from "../assets/code-pic-1.jpg";
+import codePic2 from "../assets/code-pic-2.jpg";
 
 function Hero() {
+  const carouselArray = [codePic, codePic1, codePic2];
+  const [nextCarouselSlide, setNextCarouselSlide] = useState(carouselArray[2]);
+  let arrayIndex = 0;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (arrayIndex == carouselArray.length - 1) {
+        arrayIndex = 0;
+      } else {
+        setNextCarouselSlide(carouselArray[arrayIndex]);
+        arrayIndex++;
+      }
+    }, 6000);
+  }, [nextCarouselSlide]);
+
   return (
-    <div id="hero-container" className="h-1/2">
+    <div id="hero-container" className="h-2/3">
       <div
         id="hero-words"
-        className="h-2/5 flex-col text-left pt-6 pl-6 bg-gradient-to-tr from-gray-600 to-gray-900"
+        className="h-1/2 flex-col text-left pt-6 pl-6 bg-gradient-to-tr from-gray-600 to-gray-900"
       >
         <h1 className="text-4xl font-bold pb-3">
           Hi, I'm{" "}
@@ -17,10 +34,10 @@ function Hero() {
         <h2 className="text-xl font-semibold pl-4">Web Developer</h2>
         <h3 className="text-sm font-medium pl-4">Front-End | Back-End</h3>
       </div>
-      <div id="hero-object" className="h-2/3 mx-4 mt-6 ">
+      <div id="hero-object" className="h-1/2 mx-4 mt-6 ">
         <div className="m-2 h-60 rounded-xl bg-white flex justify-center items-center ">
           <img
-            src={codePic}
+            src={nextCarouselSlide}
             alt="picture of coding screen"
             className="rounded-xl w-[99%] h-[99%]"
           />
