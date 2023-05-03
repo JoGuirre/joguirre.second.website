a22b7bcfa9e7ec58275e75eb50b84c38b97be532
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-05-03 11:21:01 -0700

    added custom colors to tailwind config

--- better-website/tailwind.config.js
+++ better-website/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        firstcolor: "faf3dd",
+        secondcolor: "c8d5b9",
+        thirdcolor: "8fc0a9",
+        fourthcolor: "68b0ab",
+        fifthcolor: "696d7d",
+      },
+    },
   },
   plugins: [],
 };
