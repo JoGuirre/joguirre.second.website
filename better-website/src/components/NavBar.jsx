d652fcd99ad54ce6d2e70ebdf35d7a07a87a2ea1
JoGuirre <41601927+JoGuirre@users.noreply.github.com>
2023-05-04 16:48:38 -0700

    plz

--- better-website/src/components/NavBar.jsx
+++ better-website/src/components/NavBar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function NavBar() {
   return (
-    <div className="sticky top-[91%] p-4 pb-8 z-50 bg-bgcolor bg-opacity-20 rounded-md backdrop-blur-lg">
+    <div className="sticky flex justify-center top-[91vh] p-4 pb-8 z-50 bg-bgcolor bg-opacity-20 rounded-md backdrop-blur-lg">
       <nav
         id="navbar-container"
-        className="sticky top-[94%] mb-0 mx-12 border rounded-xl shadow-sm shadow-textcolor bg-accentcolor border-2 border-bordercolor bg-opacity-60 backdrop-blur-md z-50"
+        className="mb-0 px border rounded-xl shadow-sm shadow-textcolor bg-accentcolor border-2 border-bordercolor bg-opacity-60 backdrop-blur-md z-50"
       >
         <ul className="flex justify-center gap-10 text-xl font-semibold pt-1 pb-1">
           <NavItems link="#hero-container" title="Home" />
